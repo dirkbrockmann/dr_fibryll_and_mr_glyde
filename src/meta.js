8ac5528d24ce2670efb608db1c0a9e278b9a241e
Dirk Brockmannn <brockmann.dirk@gmail.com>
2023-12-20 12:54:56 +0100

    v1.1.0

---
+++ src/meta.js
@@ -0,0 +1,5 @@
+export default {
+  "title": "Dr. Fibryll and Mr. Glyde",
+  "subtitle": "Pulse-coupled oscillators on a lattice.",
+  "description": "The explorable illustrates spatio-temporal pattern formation in a system of pulse-coupled oscillators. The model is a generic model that yields spiral waves and target patterns depending on the parameter choices.",
+}
